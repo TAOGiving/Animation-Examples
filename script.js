30899763c0adf110f5be811b72b9926b1ce9c6de
Alex Williams <axelwill@live.com>
2024-04-13 22:50:24 +0100

    GSAP from and fromTo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // GSAP To
 
-gsap.to(".star", { x: 400, duration: 3 });
+gsap.to(".star", { x: 400, backgroundColor: "blue", duration: 3 });
 gsap.to(".star2", { x: 400, duration: 15 });
 gsap.to(".star3", { x: -400, duration: 15 });
 gsap.to(".star4", { x: 400, y: -200, scale: 3, duration: 3 });
@@ -9,3 +9,8 @@ gsap.to(".star6", { skewX: 50, duration: 3 });
 gsap.to(".star7", { skewY: 50, duration: 3 });
 gsap.to(".star8", { scaleX: 2, duration: 3 });
 gsap.to(".star9", { scaleY: 2, duration: 3 });
+
+//GSAP from
+
+gsap.from(".star10", { x: 400, backgroundColor: "blue", duration: 3 });
+gsap.fromTo(".star11", { x: 200, y: 200 }, { x: 400, y: 0, duration: 3 });
