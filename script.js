6f3132b01d155bf996fa2797cf64d5db722fa86f
Alex Williams <axelwill@live.com>
2024-04-13 22:38:06 +0100

    further simple animations skew scale

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// GSAP To
+
 gsap.to(".star", { x: 400, duration: 3 });
 gsap.to(".star2", { x: 400, duration: 15 });
 gsap.to(".star3", { x: -400, duration: 15 });
+gsap.to(".star4", { x: 400, y: -200, scale: 3, duration: 3 });
+gsap.to(".star5", { x: 600, rotate: 270, duration: 3 });
+gsap.to(".star6", { skewX: 50, duration: 3 });
+gsap.to(".star7", { skewY: 50, duration: 3 });
+gsap.to(".star8", { scaleX: 2, duration: 3 });
+gsap.to(".star9", { scaleY: 2, duration: 3 });
