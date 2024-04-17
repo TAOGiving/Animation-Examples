9c5013e0a1d1cde2977f2db110f8be31c69baed7
Alex Williams <axelwill@live.com>
2024-04-17 22:41:59 +0100

    Bug fixed.... capital on ScrollTrigger... FML

---
+++ scriptScroll.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+//GSAP animation
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".animated-element",
+    start: "top center",
+    end: "bottom center",
+    scrub: true,
+    markers: true,
+  },
+});
+
+tl.to(".animated-element", { x: 800, rotation: 360 });
+
+//Scroll Smother from Github search Lenis
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
